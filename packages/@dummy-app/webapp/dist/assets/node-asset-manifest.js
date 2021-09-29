98128fd56d40aa644b470916d116407b690566d9
Richard Garcia <g.richard.l@hotmail.com>
2021-09-29 15:31:28 -0400

    Remove dist

--- packages/@dummy-app/webapp/dist/assets/node-asset-manifest.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-define('@dummy-app/webapp/config/node-asset-manifest', function() {
-  return {
-    default: {"bundles":{"dummyeng":{"assets":[]}}}
-  };
-});
--- packages/@dummy-app/webapp/dist/engines-dist/dummyeng/config/environment.js
+++
@@ -1,5 +0,0 @@
-define('dummyeng/config/environment', function() {
-  return {
-    default: {"modulePrefix":"@dummy-engines/dummyeng","podModulePrefix":"dummyeng/ui","EmberENV":{"EXTEND_PROTOTYPES":{"Date":false,"Function":true,"Array":true,"String":true}}}
-  };
-});
